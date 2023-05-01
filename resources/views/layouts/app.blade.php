c3a311a7ccf4559c580fe7b541b27871a6d5ab80
Данил Алексеевич Зайцев <101890973+DanteAlegori@users.noreply.github.com>
2023-05-01 23:22:02 +0600

    new visiom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('Хранилище-знаний.ru') }}</title>
+    <title>{{ config('Agrobio.ru') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -17,34 +17,30 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary orange-400">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-success p-2 text-dark bg-opacity-10">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('index')}}">Agrobio</a>
+    <a class="navbar-brand" href="{{route('index')}}">{{ __('Agrobio') }}</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route ('onas')}}">О нас</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route ('contacts')}}">Наши контакты</a>
+          <a class="nav-link active" aria-current="page" href="{{route ('onas')}}">{{ __('О нас') }}</a>
         </li>
         <li>
-          <a class="nav-link active" aria-current="page" href="{{route ('allbio')}}">Биографии</a>
+          <a class="nav-link active" aria-current="page" href="{{route ('allbio')}}">{{ __('Биографии') }}</a>
         </li>
         <li>
-          <a class="nav-link active" aria-current="page" href="{{route ('news')}}">Новости</a>
+          <a class="nav-link active" aria-current="page" href="{{route ('news')}}">{{ __('Новости') }}</a>
         </li>
         @Auth
         <li>
-          <a class="nav-link active" aria-current="page" href="{{route ('news')}}">Избраное</a>
+          <a class="nav-link active" aria-current="page" href="{{route ('news')}}">{{ __('Избраное') }}</a>
         </li>
         @endAuth
-        <ul class="navbar-nav ms-auto">
-          <!-- Authentication Links -->
-          @guest
+
+        @guest
               @if (Route::has('login'))
                   <li class="nav-item">
                       <a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>
@@ -57,11 +53,7 @@
                   </li>
               @endif
           @else
-          <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-  
+          
               <li class="nav-item dropdown">
                   <a id="navbarDropdown" class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                       {{ Auth::user()->name }}
@@ -73,7 +65,7 @@
                                        document.getElementById('logout-form').submit();">
                           {{ __('Выйти из акаунта') }}
                       </a>
-                        <a class="nav-link active" aria-current="page" href="{{route ('user')}}">Профиль</a>
+                        <a class="nav-link active" aria-current="page" href="{{route ('user')}}">{{ __('Профиль') }}</a>
 
                         
                       <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -82,6 +74,9 @@
                   </div>
               </li>
           @endguest
+        <ul class="navbar-nav ms-auto">
+          <!-- Authentication Links -->
+          
       </ul>
 </li>
 </ul>
@@ -93,18 +88,19 @@
 </nav>
 
 
-<main class="py-4 bg-orange-400">
+<main class="py-4 ">
             @yield('content')
         </main>
 
-        <div class="container  bf-orange-400" >
+        <div class="container  " >
   <footer class="py-3 my-4 ">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Главная</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Новости</a></li>
       <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">О нас</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Где нас найти</a></li>
+
     </ul>
-    <p class="text-center text-muted">© 2023 Company, Inc Dante Alegori</p>
+    <p class="text-center text-muted">© 2023  Dante Alegori</p>
   </footer>
 </div>
 </body>
