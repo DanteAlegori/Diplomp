0ab70c3bebf9a1590bf8cb37b8cb5616bd3905a9
Данил Алексеевич Зайцев <101890973+DanteAlegori@users.noreply.github.com>
2023-05-15 04:58:27 +0600

    sinhron

--- resources/views/static/onas.blade.php
+++ resources/views/static/onas.blade.php
@@ -20,8 +20,6 @@
           <li class="mb-3"><i class="fa fa-phone mr-2"></i>+7-950-334-56-65</li>
           <li class="mb-3"><i class="fa fa-envelope mr-2"></i>poshta@gmail.com</li>
           <li>
-            <i class="fa fa-map-marker mr-2"></i>
-            <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2287.11704836513!2d73.30900699865637!3d55.02364100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43ab003bd31df837%3A0x7c75d7c66435e039!2z0JPQvtGA0L3QsNGPINGD0LsuLCA50JAsINCe0LzRgdC6LCDQntC80YHQutCw0Y8g0L7QsdC7LiwgNjQ0MDA4!5e0!3m2!1sru!2sru!4v1680693794844!5m2!1sru!2sru">Ростов-на-Дону, ул.Шолохова, 64</a>
           </li>
         </ul>
         <div class="mb-5">
@@ -31,3 +29,4 @@
     </div>
   </div>
 @endsection
+
